dfe9f3fe12e6ff6651516ef5855177620fa6e900
Luís <luis@luisonthekeyboard.com>
2023-10-14 23:29:57 +0200

    Add base archetype

---
+++ src/main/java/rs/luis/App.java
@@ -0,0 +1,13 @@
+package rs.luis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
