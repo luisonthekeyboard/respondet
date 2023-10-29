added90e8c534ba0576ec431326b034f473e140c
Luís <luis@luisonthekeyboard.com>
2023-10-29 11:42:29 +0100

    Use spring shell to drive the execution; Use Gradle to build.

---
+++ src/test/java/rs/luis/respondet/RespondetApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.luis.respondet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RespondetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
